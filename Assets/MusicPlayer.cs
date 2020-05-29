94e1eb99fd7968073cee8fbbe78ad2619f54edb0
Andrei Octavian Minculescu <andrei.omul11@gmail.com>
2020-05-29 17:16:07 +0300

    Commit 3

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int numMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayer > 1 && currentScene < 11)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
